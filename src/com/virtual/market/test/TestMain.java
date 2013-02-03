06684378bc64899ada4bbb22b640ee6adbe28126
Mohamed Sami <melsatar@gmail.com>
2013-02-03 22:08:03 +0200

    adding Sign up test case

--- src/com/virtual/market/test/TestMain.java
+++ src/com/virtual/market/test/TestMain.java
@@ -29,65 +29,30 @@ public class TestMain extends ActivityInstrumentationTestCase2<UserFirstActivity
 		solo.finishOpenedActivities();
 	}
 
+	public void testSignUp() throws Exception {
 
-/*	public void testAddNote() throws Exception {
-		solo.clickOnMenuItem("Add note");
-		//Assert that NoteEditor activity is opened
-		solo.assertCurrentActivity("Expected NoteEditor activity", "NoteEditor"); 
-		//In text field 0, add Note 1
-		solo.enterText(0, "Note 1");
-		solo.goBack(); 
-		//Clicks on menu item
-		solo.clickOnMenuItem("Add note");
-		//In text field 0, add Note 2
-		solo.enterText(0, "Note 2");
-		//Go back to first activity named "NotesList"
-		solo.goBackToActivity("NotesList"); 
-		//Takes a screenshot and saves it in "/sdcard/Robotium-Screenshots/".
-		solo.takeScreenshot();
-		boolean expected = true;
-		boolean actual = solo.searchText("Note 1") && solo.searchText("Note 2");
-		//Assert that Note 1 & Note 2 are found
-		assertEquals("Note 1 and/or Note 2 are not found", expected, actual); 
-
-	}
-
-	public void testEditNote() throws Exception {
-		// Click on the second list line
-		solo.clickInList(2); 
-		// Change orientation of activity
-		solo.setActivityOrientation(Solo.LANDSCAPE);
-		// Change title
-		solo.clickOnMenuItem("Edit title");
-		//In first text field (0), add test
-		solo.enterText(0, " test");  
+		solo.clickOnButton("Register");
+		solo.assertCurrentActivity("Expected Log in activity", "User_Signup");
+		solo.enterText(0, "melsatar2013");
+		solo.enterText(1, "melsatar@ymail.com");
+		solo.enterText(2, "123");
+		solo.enterText(3, "100200300400");
+		solo.getCurrentSpinners();
+		solo.sleep(1500);
+		solo.pressSpinnerItem(0, 0);
+		solo.sleep(1000);
 		solo.goBack();
-		boolean expected = true;
-		// (Regexp) case insensitive
-		boolean actual = solo.waitForText("(?i).*?note 1 test"); 
-		//Assert that Note 1 test is found
-		assertEquals("Note 1 test is not found", expected, actual); 
-
+		solo.sleep(1500);
+		solo.pressSpinnerItem(1, 0);
+		solo.sleep(1000);
+		solo.goBack();
+		solo.enterText(4, "111 Building, 66 Flat");
+		solo.clickOnButton("Register");
+		solo.waitForActivity("SMSValidate");
+		solo.assertCurrentActivity("Expected SMA vaildate activity", "SMSValidate");
+		solo.enterText(0, "1111");
+		solo.clickOnButton("Validate");
 	}
-
-	public void testRemoveNote() throws Exception {
-		//(Regexp) case insensitive/text that contains "test"
-		solo.clickOnText("(?i).*?test.*");
-		//Delete Note 1 test
-		solo.clickOnMenuItem("Delete");
-		//Note 1 test & Note 2 should not be found
-		boolean expected = false;   
-		boolean actual = solo.searchText("Note 1 test");
-		//Assert that Note 1 test is not found
-		assertEquals("Note 1 Test is found", expected, actual);  
-		solo.clickLongOnText("Note 2");
-		//Clicks on Delete in the context menu
-		solo.clickOnText("Delete");  
-		actual = solo.searchText("Note 2");
-		//Assert that Note 2 is not found
-		assertEquals("Note 2 is found", expected, actual);  
-	}
-*/
 	public void testSignIn() throws Exception {
 
 		solo.clickOnButton("Log In");
@@ -96,16 +61,15 @@ public class TestMain extends ActivityInstrumentationTestCase2<UserFirstActivity
 		solo.enterText(1, "123");
 		solo.clickOnButton("Log In");
 		solo.waitForActivity("User_MainActivity");
-		solo.assertCurrentActivity("Expected User Manin activity", "User_MainActivity");
-		 
+		solo.assertCurrentActivity("Expected User Manin activity", "User_MainActivity"); 
 	}
+	
+	
 	public void testViewCart() throws Exception {
-
 		solo.clickOnButton("View myCart");
 		solo.assertCurrentActivity("Expected Shopping cart activity", "Shopping_cart");
 		solo.waitForActivity("Shopping_cart");
 		solo.clickOnButton("checkout");		 
 	}
-
 
 }
